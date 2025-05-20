7e8fd5658c8ffeb567c086fb5a49c82e246723b0
Simon Dutal <simon.dutal@epitech.eu>
2025-05-20 10:34:56 +0200

    [~]moved initialization functions to initialization

--- src/initialization/create_menu_sprites.c
+++ src/initialization/create_menu_sprites.c
@@ -7,7 +7,30 @@
 
 #include "wolf3d.h"
 
-bool create_gui_sprite(data_t *data, const char *filename,
+static void set_position_and_scale(float scale_value, float positionx,
+    float positiony, sfSprite *sprite)
+{
+    sfVector2f scale = {scale_value, scale_value};
+    sfVector2f position = {positionx, positiony};
+
+    sfSprite_setPosition(sprite, position);
+    sfSprite_setScale(sprite, scale);
+    return;
+}
+
+static void set_sprite_positions(data_t *data)
+{
+    set_position_and_scale(1.38, 0, 0, data->sprites.menu.background);
+    set_position_and_scale(0.5, 250, 175, data->sprites.menu.new_game);
+    set_position_and_scale(0.375, 290, 260, data->sprites.menu.continue_game);
+    set_position_and_scale(0.45, 290, 290, data->sprites.menu.options);
+    set_position_and_scale(0.425, 300, 355, data->sprites.menu.lore);
+    set_position_and_scale(0.3, 315, 460, data->sprites.menu.exit_game);
+    set_position_and_scale(0.8, 0, 0, data->sprites.menu.title);
+    return;
+}
+
+static bool create_gui_sprite(data_t *data, const char *filename,
     sfSprite **adress_sprite)
 {
     gui_texture_t **gui_txt = &data->assets.gui_textures;
@@ -17,7 +40,7 @@ bool create_gui_sprite(data_t *data, const char *filename,
     if (!sprite)
         return true;
     for (; gui != NULL && my_strcmp(gui->name, filename) != 0;
-         gui = gui->next);
+        gui = gui->next);
     if (gui == NULL) {
         if (data->arguments.debug)
             printf("Failed to load %s sprite. Aborting\n", filename);
@@ -31,43 +54,26 @@ bool create_gui_sprite(data_t *data, const char *filename,
     return false;
 }
 
-bool create_options_sprite(data_t *data)
-{
-    if (create_gui_sprite(data, "empty_box.png",
-        &data->sprites.menu.options_sprites.empty_box))
-        return true;
-    if (create_gui_sprite(data, "full_screen.png",
-        &data->sprites.menu.options_sprites.full_screen))
-        return true;
-    if (create_gui_sprite(data, "full_box.png",
-        &data->sprites.menu.options_sprites.full_box))
-        return true;
-    if (create_gui_sprite(data, "back.png",
-        &data->sprites.menu.options_sprites.back))
-        return true;
-    return false;
-}
-
 bool create_menu_sprites(data_t *data)
 {
     if (create_gui_sprite(data, "menu_background.png",
-        &data->sprites.menu.background))
+        &data->sprites.menu.background) ||
+    create_gui_sprite(data, "new_game.png", &data->sprites.menu.new_game) ||
+    create_gui_sprite(data, "continue_game.png",
+        &data->sprites.menu.continue_game) ||
+    create_gui_sprite(data, "lore.png", &data->sprites.menu.lore) ||
+    create_gui_sprite(data, "exit.png", &data->sprites.menu.exit_game) ||
+    create_gui_sprite(data, "option.png", &data->sprites.menu.options) ||
+    create_gui_sprite(data, "title.png", &data->sprites.menu.title) ||
+    create_gui_sprite(data, "empty_box.png",
+        &data->sprites.menu.options_sprites.empty_box) ||
+    create_gui_sprite(data, "full_screen.png",
+        &data->sprites.menu.options_sprites.full_screen) ||
+    create_gui_sprite(data, "full_box.png",
+        &data->sprites.menu.options_sprites.full_box) ||
+    create_gui_sprite(data, "back.png",
+        &data->sprites.menu.options_sprites.back))
         return true;
-    if (create_gui_sprite(data, "new_game.png",
-        &data->sprites.menu.new_game))
-        return true;
-    if (create_gui_sprite(data, "continue_game.png",
-        &data->sprites.menu.continue_game))
-        return true;
-    if (create_gui_sprite(data, "lore.png", &data->sprites.menu.lore))
-        return true;
-    if (create_gui_sprite(data, "exit.png", &data->sprites.menu.exit_game))
-        return true;
-    if (create_gui_sprite(data, "option.png", &data->sprites.menu.options))
-        return true;
-    if (create_gui_sprite(data, "title.png", &data->sprites.menu.title))
-        return true;
-    if (create_options_sprite(data))
-        return true;
+    set_sprite_positions(data);
     return false;
 }
--- src/main_loop/run_loop.c
+++ src/main_loop/run_loop.c
@@ -7,29 +7,6 @@
 
 #include "wolf3d.h"
 
-void set_position_scale(float scale_value, float positionx,
-    float positiony, sfSprite *sprite)
-{
-    sfVector2f scale = {scale_value, scale_value};
-    sfVector2f position = {positionx, positiony};
-
-    sfSprite_setPosition(sprite, position);
-    sfSprite_setScale(sprite, scale);
-    return;
-}
-
-void set_all(data_t *data)
-{
-    set_position_scale(1.38, 0, 0, data->sprites.menu.background);
-    set_position_scale(0.5, 250, 175, data->sprites.menu.new_game);
-    set_position_scale(0.375, 290, 260, data->sprites.menu.continue_game);
-    set_position_scale(0.45, 290, 290, data->sprites.menu.options);
-    set_position_scale(0.425, 300, 355, data->sprites.menu.lore);
-    set_position_scale(0.3, 315, 460, data->sprites.menu.exit_game);
-    set_position_scale(0.8, 0, 0, data->sprites.menu.title);
-    return;
-}
-
 void run_loop(data_t *data)
 {
     sfEvent event;
@@ -37,7 +14,6 @@ void run_loop(data_t *data)
     sfVector2i mouse_pos = {0};
     sfVector2f newgame_pos = {0};
 
-    set_all(data);
     newgame_pos = sfSprite_getPosition(data->sprites.menu.new_game);
     while (sfRenderWindow_isOpen(data->window)) {
         sfRenderWindow_display(data->window);
