18f83d586cf71cb8eed0d26eddcf959194182da8
baptiste-jaunais <baptiste.jaunais@epitech.eu>
2025-05-22 10:35:04 +0200

    [+] added handle_pause_interactions.c

---
+++ src/main_loop/pause/handle_pause_interactions.c
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2025
+** wolf3d
+** File description:
+** handle_menu_interactions
+*/
+
+#include "wolf3d.h"
+
+static void detect_button_mouse_click(data_t *data, pause_infos_t *pause_infos)
+{
+    if (sfFloatRect_contains(&pause_infos->options_infos,
+    pause_infos->mouse_pos.x, pause_infos->mouse_pos.y))
+        printf("options\n");
+    if (sfFloatRect_contains(&pause_infos->exit_infos,
+    pause_infos->mouse_pos.x, pause_infos->mouse_pos.y))
+        data->exit = true;
+}
+
+static void handle_button_clicks(data_t *data)
+{
+    pause_infos_t pause_infos = {{0}, {0}, {0}};
+
+    pause_infos.mouse_pos =
+    sfMouse_getPositionRenderWindow(data->window);
+    pause_infos.options_infos =
+    sfSprite_getGlobalBounds(data->sprites.menu.options);
+    pause_infos.exit_infos =
+    sfSprite_getGlobalBounds(data->sprites.menu.exit_game);
+    detect_button_mouse_click(data, &pause_infos);
+}
+
+void handle_pause_interactions(data_t *data)
+{
+    if (data->event.mouseButton.button == sfMouseLeft)
+            handle_button_clicks(data);
+}
