1045b11532bdb2479f3c3f7cad551f2d8359a263
Simon Dutal <simon.dutal@epitech.eu>
2025-06-12 12:01:44 +0200

    [+]game loading now loads weapon ammo and position correctly

--- src/main_loop/menu/load_game.c
+++ src/main_loop/menu/load_game.c
@@ -9,6 +9,8 @@
 
 static void load_player_data(data_t *data, char **lines)
 {
+    printf("  map is %s.\n", lines[7]);
+    set_new_map(data, lines[7]);
     data->player.health = atof(lines[0]);
     data->player.x = atof(lines[1]);
     data->player.y = atof(lines[2]);
@@ -16,7 +18,27 @@ static void load_player_data(data_t *data, char **lines)
     data->player.dirY = atof(lines[4]);
     data->player.planeX = atof(lines[5]);
     data->player.planeY = atof(lines[6]);
-    set_new_map(data, lines[7]);
+}
+
+static void set_weapon_ammo(data_t *data, char *name, char *ammo_str)
+{
+    if (name == NULL || ammo_str == NULL)
+        return;
+    printf("  loading weapon %s with %i ammo.\n", name, atoi(ammo_str));
+    for (weapon_t *weapon = data->assets.weapons;
+        weapon != NULL; weapon = weapon->next) {
+        if (my_strcmp(name, weapon->name) == 0) {
+            weapon->ammo = atoi(ammo_str);
+            return;
+        }
+    }
+}
+
+static void load_weapons_ammo(data_t *data, char **lines)
+{
+    for (int i = 10; i < my_ptrarraylen((void **) lines); i += 7) {
+        set_weapon_ammo(data, lines[i - 2], lines[i]);
+    }
 }
 
 void load_game(data_t *data)
@@ -35,6 +57,7 @@ void load_game(data_t *data)
         return;
     }
     load_player_data(data, lines);
+    load_weapons_ammo(data, lines);
     my_free(buffer);
     my_freestrarray(lines);
 }
