b9cd2c36ec8bff5bf2be21761d1b85df4904109d
Simon Dutal <simon.dutal@epitech.eu>
2025-06-12 10:39:00 +0200

    [~]unit test fix for debuging

--- tests/test_wolf3d.c
+++ tests/test_wolf3d.c
@@ -15,9 +15,10 @@ void redirect_all_std(void)
     cr_redirect_stderr();
 }
 
-Test(wolf3d, initialize_data, .init = redirect_all_std)
+Test(wolf3d, initialize_data)
 {
     data_t data = initialize_data();
+    data.arguments.debug = true;
     
     cr_assert(initialize_game(&data) == false);
     terminate_game(&data);
