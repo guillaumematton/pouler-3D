36fda9c12e22a7438dde21bf9a29c84d1fb4da4f
Simon Dutal <simon.dutal@epitech.eu>
2025-06-24 12:05:30 +0200

    [+]added foward and backward dust particule animation

--- src/main_loop/game/render_dust_particules.c
+++ src/main_loop/game/render_dust_particules.c
@@ -7,43 +7,123 @@
 
 #include "wolf3d.h"
 
-
-static void move_dust_particule(data_t *data, uint i)
+static void handle_player_movement_dust_particules_strafing(data_t *data)
 {
-    data->dust_particules[i].x_pos += data->dust_particules[i].x_vel
-        * data->tick_duration * GAME_FPS;
-    data->dust_particules[i].y_pos += data->dust_particules[i].y_vel
-        * data->tick_duration * GAME_FPS;
-}
-
-static void respawn_dust_particule(data_t *data, uint i)
-{
-    if (data->dust_particules[i].x_pos <= 0 ||
-        data->dust_particules[i].x_pos >= INTERNAL_WIDTH ||
-        data->dust_particules[i].y_pos <= 0 ||
-        data->dust_particules[i].y_pos >= INTERNAL_HEIGHT) {
-        data->dust_particules[i].x_pos = rand() % INTERNAL_WIDTH;
-        data->dust_particules[i].y_pos = rand() % INTERNAL_HEIGHT;
-        data->dust_particules[i].x_vel = (rand() % 20 - 10) / 10.0;
-        data->dust_particules[i].y_vel = (rand() % 20 - 10) / 10.0;
+    if (sfKeyboard_isKeyPressed(sfKeyD)) {
+        for (uint i = 0; i < DUST_PARTICULES_COUNT; i++) {
+            data->dust_particules[i].x_pos += -1
+                * data->tick_duration * 1000;
+        }
+    }
+    if (sfKeyboard_isKeyPressed(sfKeyQ)) {
+        for (uint i = 0; i < DUST_PARTICULES_COUNT; i++) {
+            data->dust_particules[i].x_pos += 1
+                * data->tick_duration * 1000;
+        }
     }
 }
 
-static void render_dust_particule(data_t *data, uint i)
+static void handle_player_movement_dust_particules_rotation(data_t *data)
+{
+    if (sfKeyboard_isKeyPressed(sfKeyE)) {
+        for (uint i = 0; i < DUST_PARTICULES_COUNT; i++) {
+            data->dust_particules[i].x_pos -=
+            (180 / (0.2f * data->tick_duration)) / INTERNAL_WIDTH;
+        }
+    }
+    if (sfKeyboard_isKeyPressed(sfKeyA)) {
+        for (uint i = 0; i < DUST_PARTICULES_COUNT; i++) {
+            data->dust_particules[i].x_pos +=
+            (180 / (0.2f * data->tick_duration)) / INTERNAL_WIDTH;
+        }
+    }
+}
+
+static void check_screen_center_particule_colision(data_t *data, uint i)
+{
+    if (fabs((data->dust_particules[i].x_pos - (INTERNAL_WIDTH / 2)) *
+        (0.2f * data->tick_duration) * 9)
+        + fabs((data->dust_particules[i].y_pos - (INTERNAL_HEIGHT / 2)) *
+        (0.2f * data->tick_duration) * 9) <= 3) {
+        data->dust_particules[i].x_pos = rand() % INTERNAL_WIDTH;
+        data->dust_particules[i].y_pos = rand() % INTERNAL_HEIGHT;
+        data->dust_particules[i].x_vel = (rand() % 21 - 10) / 10.0;
+        data->dust_particules[i].y_vel = (rand() % 21 - 10) / 10.0;
+    }
+}
+
+static void handle_player_movement_dust_particules_forward_backward
+(data_t *data)
+{
+    if (sfKeyboard_isKeyPressed(sfKeyZ))
+        for (uint i = 0; i < DUST_PARTICULES_COUNT; i++) {
+            data->dust_particules[i].x_pos +=
+            (data->dust_particules[i].x_pos - (INTERNAL_WIDTH / 2)) *
+            (0.2f * data->tick_duration) * 9;
+            data->dust_particules[i].y_pos +=
+            (data->dust_particules[i].y_pos - (INTERNAL_HEIGHT / 2)) *
+            (0.2f * data->tick_duration) * 9;
+        }
+    if (sfKeyboard_isKeyPressed(sfKeyS))
+        for (uint i = 0; i < DUST_PARTICULES_COUNT; i++) {
+            check_screen_center_particule_colision(data, i);
+            data->dust_particules[i].x_pos -=
+            (data->dust_particules[i].x_pos - (INTERNAL_WIDTH / 2)) *
+            (0.2f * data->tick_duration) * 9;
+            data->dust_particules[i].y_pos -=
+            (data->dust_particules[i].y_pos - (INTERNAL_HEIGHT / 2)) *
+            (0.2f * data->tick_duration) * 9;
+        }
+}
+
+static void handle_player_movement_dust_particules(data_t *data)
+{
+    handle_player_movement_dust_particules_strafing(data);
+    handle_player_movement_dust_particules_rotation(data);
+    handle_player_movement_dust_particules_forward_backward(data);
+}
+
+static void move_dust_particule(data_t *data)
+{
+    for (uint i = 0; i < DUST_PARTICULES_COUNT; i++) {
+        data->dust_particules[i].x_pos += data->dust_particules[i].x_vel
+        * data->tick_duration * GAME_FPS;
+        data->dust_particules[i].y_pos += data->dust_particules[i].y_vel
+        * data->tick_duration * GAME_FPS;
+    }
+}
+
+static void respawn_dust_particule(data_t *data)
+{
+    for (uint i = 0; i < DUST_PARTICULES_COUNT; i++) {
+        if (data->dust_particules[i].x_pos <= 0 ||
+            data->dust_particules[i].x_pos >= INTERNAL_WIDTH ||
+            data->dust_particules[i].y_pos <= 0 ||
+            data->dust_particules[i].y_pos >= INTERNAL_HEIGHT) {
+            data->dust_particules[i].x_pos = rand() % INTERNAL_WIDTH;
+            data->dust_particules[i].y_pos = rand() % INTERNAL_HEIGHT;
+            data->dust_particules[i].x_vel = (rand() % 21 - 10) / 10.0;
+            data->dust_particules[i].y_vel = (rand() % 21 - 10) / 10.0;
+        }
+    }
+}
+
+static void render_dust_particule(data_t *data)
 {
     sfColor color = sfColor_fromRGB(125, 125, 125);
 
-    sfImage_setPixel(data->game_screen_image,
+    for (uint i = 0; i < DUST_PARTICULES_COUNT; i++) {
+        sfImage_setPixel(data->game_screen_image,
         data->dust_particules[i].x_pos,
         data->dust_particules[i].y_pos,
         color);
+    }
 }
 
 void render_dust_particules(data_t *data)
 {
-    for (uint i = 0; i < DUST_PARTICULES_COUNT; i++) {
-        move_dust_particule(data, i);
-        respawn_dust_particule(data, i);
-        render_dust_particule(data, i);
-    }
+    handle_player_movement_dust_particules(data);
+    move_dust_particule(data);
+    respawn_dust_particule(data);
+    render_dust_particule(data);
 }
